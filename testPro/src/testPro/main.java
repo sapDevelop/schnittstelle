a1c2055cbde85beec169ed3a9cb8669a4bd74336
Robin <Robin@Y50-PC>
2015-05-08 16:34:08 +0200

    Angelegt

---
+++ testPro/src/testPro/main.java
@@ -0,0 +1,22 @@
+package testPro;
+import com.sap.conn.jco.JCoDestination;
+import com.sap.conn.jco.JCoDestinationManager;
+import com.sap.conn.jco.JCoException;
+
+public class main {
+
+	
+	
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hallo Welt");
+		
+		
+	}
+
+}
